6cf9723d80e9279ffde744eb123783ee2ee0001e
ZielDev <ziellucio@outlook.com>
2023-01-26 22:08:48 -0600

    Acomodando las carpetas del proyecto

--- Intermediate/00_dates.py
+++ Intermediate/00_dates.py
@@ -4,12 +4,7 @@ from datetime import datetime
 
 now = datetime.now()
 
-print(now.year)
-print(now.month)
-print(now.day)
-print(now.hour)
-print(now.minute)
-print(now.second)
+print_date(now)
 
 timestamp = now.timestamp()
 
