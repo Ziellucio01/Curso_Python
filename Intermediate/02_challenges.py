c2f23a0fee5b8d5416d4dbe69deac1c60505b806
ZielDev <ziellucio@outlook.com>
2023-01-28 15:36:26 -0600

    Reto 02 de intermedio

--- Intermediate/02_challenges.py
+++ Intermediate/02_challenges.py
@@ -4,3 +4,27 @@
 EL FAMOSO "FIZZ  BUZZ"
 """
 
+def fizzbuzz():
+    for index in range(1, 101):
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")
+        else:
+            print(index)
+
+fizzbuzz()
+
+""" 
+¿ES UN ANAGRAMA?
+"""
+
+def is_anagram(word_one, word_two):
+    if word_one.lower() == word_two.lower():
+        return False
+    elif 
+    return word_one == word_two
+
+print(is_anagram("Amor", "Roma"))
