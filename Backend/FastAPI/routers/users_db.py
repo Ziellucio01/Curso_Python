2e43e429e8db6374c1742a256ce01f4bf2a38f32
ZielDev <ziellucio@outlook.com>
2023-03-20 14:16:07 -0600

    Avanzado - Mongodb local 7:04:56 - ZielDev

--- Backend/FastAPI/routers/users_db.py
+++ Backend/FastAPI/routers/users_db.py
@@ -2,32 +2,30 @@ from fastapi import APIRouter, HTTPException, status
 from db.models.user import User
 from db.client import db_client
 from db.schemas.user import user_schema, users_shema
+from bson import ObjectId
 
 router = APIRouter(prefix="/userdb",
                    tags=["userdb"],
                    responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
 
 
-users_list = []
-
-
-@ router.get("/", response_model=list(User))
+@ router.get("/", response_model=list[User])
 async def users():
     return users_shema(db_client.local.users.find())
 
 # Path
 
 
-@ router.get("/{ide}")
-async def user(ide: int):
-    return searh_user("_id", user.email)
+@ router.get("/{id}")
+async def user(id: str):
+    return searh_user("_id", ObjectId(id))
 
 # Query http://127.0.0.1:8000/user/?id=1
 
 
 @ router.get("/")
-async def userget():
-    return db_client.local.users.find()
+async def userget(id: str):
+    return searh_user("_id", ObjectId(id))
 
 
 @ router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
@@ -45,32 +43,24 @@ async def user_post(user: User):
     return User(**new_user)
 
 
-@ router.put("/")
+@ router.put("/", response_model=User)
 async def user_put(user: User):
 
     found = False
+    user_dict = dict(user)
+    del user_dict["id"]
+    try:
+        db_client.local.users.find_one_and_replace(
+            {"_id": ObjectId(user.id)}, user_dict)
+    except:
+        return {"error": "No se ha actualizado el usuario"}
 
-    for index, saved_user in enumerate(users_list):
-        if saved_user.id == user.id:
-            users_list[index] = user
-            found = True
-
-    if not found:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="No se ha actualizado el usario")
-        # return {"error": "No se ha actualizado el usuario"}
-    else:
-        return user
+    return searh_user("_id", ObjectId(user.id))
 
 
-@router.delete("/{id}")
-async def user_del(id: int):
-    found = False
-    for index, saved_user in enumerate(users_list):
-        if saved_user.id == id:
-            del users_list[index]
-            found = True
-            return {"mensaje": "El usuario se elimino correctamente"}
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def user_del(id: str):
+    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
 
     if not found:
         raise HTTPException(
@@ -78,7 +68,7 @@ async def user_del(id: int):
         # return {"error": "No se ha eliminado el usuario"}
 
 
-def searh_user(field: str, key: str):
+def searh_user(field: str, key):
     try:
         user = db_client.local.users.find_one({field: key})
         return User(**user_schema(user))
