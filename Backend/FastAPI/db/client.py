40b267577be0b8878ec3aceb6036ba5b7e22866f
ZielDev <ziellucio@outlook.com>
2023-03-20 15:21:21 -0600

    Avanzado - Mongodb remoto 7:40:00 - ZielDev

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,3 +1,9 @@
 from pymongo import MongoClient
 
-db_client = MongoClient()
+# db_client = MongoClient().local # Base de datos local
+
+
+#  Base de datos remota
+
+db_client = MongoClient(
+    "mongodb+srv://test:test@cluster0.hfm52kp.mongodb.net/?retryWrites=true&w=majority").test
